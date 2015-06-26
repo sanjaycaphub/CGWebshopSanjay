ee425ab3c7e992dbb23e3248257888df05a63549
sanjaycaphub <sanjay.sukheja@capgemini.com>
2015-06-26 18:49:33 +0530

    sprint 2 commit

--- Webshop-Root/WebshopServices/src/main/java/com/cg/petsupply/repository/impl/UserRepositoryImpl.java
+++ Webshop-Root/WebshopServices/src/main/java/com/cg/petsupply/repository/impl/UserRepositoryImpl.java
@@ -10,6 +10,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cg.petsupply.model.User;
 import com.cg.petsupply.repository.IUserRepository;
@@ -24,20 +25,57 @@ public class UserRepositoryImpl implements IUserRepository {
 
 	@PersistenceContext
 	private EntityManager em;
-	
-	/* (non-Javadoc)
-	 * @see com.cg.petsupply.repository.IUserRepository#authenticateUser(java.lang.String, java.lang.String)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.cg.petsupply.repository.IUserRepository#authenticateUser(java.lang
+	 * .String, java.lang.String)
 	 */
 	@SuppressWarnings("unchecked")
-	public List<User> authenticateUser(String userName, String password, String userRole) {
-		
-		Query query = em.createQuery("select u from User u where lower(u.userName) like lower(:username) and u.password like :passwd "
-									+ "and u.userRole like :userrole");
-		query.setParameter("username", userName); 
+	public List<User> authenticateAdminAndUser(String userName,
+			String password, String isAdmin) {
+		Query query = null;
+		if (isAdmin.equalsIgnoreCase("Y")) {
+			query = em.createNamedQuery("authenticateAdmin");
+			query.setParameter("isadmin", isAdmin);
+		} else
+			query = em.createNamedQuery("authenticateUser");
+
+		query.setParameter("username", userName);
 		query.setParameter("passwd", password);
-		query.setParameter("userrole", userRole);
-		
-		return query.getResultList();		
+
+		return query.getResultList();
+	}
+
+	@Transactional
+	public boolean addNewUser(User user) {
+		boolean returnVar = false;
+
+		if (checkUserNameExists(user.getUserName())
+				|| checkEmailExists(user.getEmail()))
+			return returnVar;
+		else {
+			em.persist(user);
+			returnVar = true;
+		}
+
+		return returnVar;
+	}
+
+	private boolean checkUserNameExists(String userName) {
+		Query query = em
+				.createQuery("select u from User u where lower(u.userName) like lower(:userName)");
+		query.setParameter("userName", userName);
+		return query.getResultList().size() > 0;
+	}
+
+	private boolean checkEmailExists(String email) {
+		Query query = em
+				.createQuery("select u from User u where lower(u.email) like lower(:email)");
+		query.setParameter("email", email);
+		return query.getResultList().size() > 0;
 	}
 
 }
