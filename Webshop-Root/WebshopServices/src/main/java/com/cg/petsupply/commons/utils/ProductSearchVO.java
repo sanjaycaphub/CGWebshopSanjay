d44eac8d10c01c947d8c05139f3b63bdd3bb39bf
sanjaycaphub <sanjay.sukheja@capgemini.com>
2015-06-26 18:27:28 +0530

    sprint 2 commit

---
+++ Webshop-Root/WebshopServices/src/main/java/com/cg/petsupply/commons/utils/ProductSearchVO.java
@@ -0,0 +1,63 @@
+package com.cg.petsupply.commons.utils;
+
+/**
+ * 
+ */
+
+/**
+ * @author ssukheja
+ *
+ */
+public class ProductSearchVO {
+
+	private String prodName;
+
+	private String prodDesc;
+
+	private Long categoryId;
+
+	/**
+	 * @return the prodName
+	 */
+	public String getProdName() {
+		return prodName;
+	}
+
+	/**
+	 * @param prodName
+	 *            the prodName to set
+	 */
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+
+	/**
+	 * @return the prodDesc
+	 */
+	public String getProdDesc() {
+		return prodDesc;
+	}
+
+	/**
+	 * @param prodDesc
+	 *            the prodDesc to set
+	 */
+	public void setProdDesc(String prodDesc) {
+		this.prodDesc = prodDesc;
+	}
+
+	/**
+	 * @return the categoryId
+	 */
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	/**
+	 * @param categoryId
+	 *            the categoryId to set
+	 */
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+}
