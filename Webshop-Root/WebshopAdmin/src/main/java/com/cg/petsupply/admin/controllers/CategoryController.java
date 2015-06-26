6effbe192869106b9c2e9c75277dfa6c3d6eaaab
sanjaycaphub <sanjay.sukheja@capgemini.com>
2015-06-26 18:34:54 +0530

    Sprint 2 commit

--- Webshop-Root/WebshopAdmin/src/main/java/com/cg/petsupply/admin/controllers/CategoryController.java
+++ Webshop-Root/WebshopAdmin/src/main/java/com/cg/petsupply/admin/controllers/CategoryController.java
@@ -3,8 +3,6 @@
  */
 package com.cg.petsupply.admin.controllers;
 
-
-
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
@@ -24,79 +22,86 @@ import com.cg.petsupply.model.Category;
 import com.cg.petsupply.service.ICategoryService;
 
 /**
- * @author ssukheja
  * Request controller for admin to manage Categories
+ * 
+ * @author ssukheja
  */
 
 @Controller
 public class CategoryController {
 
-	@Autowired	
+	@Autowired
 	private ICategoryService categoryService;
-	
-	private static final Logger log = Logger.getLogger(CategoryController.class);
+
+	private static final Logger log = Logger
+			.getLogger(CategoryController.class);
 
 	/**
 	 * Handler Mapping/Method for Admin to add new category
+	 * 
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping(value = "/addcategory.htm", method = RequestMethod.GET)	
+	@RequestMapping(value = "/addcategory.htm", method = RequestMethod.GET)
 	public String addCategory(ModelMap model, HttpServletRequest request) {
 
-		if(SessionValidateUtil.invalidSession(request))
-			return "login";
-		
-		log.info("Starting to add new Category");		
-		
+		if (SessionValidateUtil.invalidSession(request))
+			return Constants.returnLogin;
+
+		log.info("Starting to add new Category");
+
 		model.addAttribute("categoryForm", new Category());
-		return "addcategory";
-	}	
+		return Constants.returnAddCategory;
+	}
 
 	/**
 	 * Handler Mapping/Method to provide list of all existing categories
+	 * 
 	 * @param model
 	 * @return
 	 */
 	@RequestMapping(value = "/allcategories.htm", method = RequestMethod.GET)
 	public String showAllCategories(ModelMap model, HttpServletRequest request) {
-		
-		if(SessionValidateUtil.invalidSession(request))
-			return "login";
-		
+
+		if (SessionValidateUtil.invalidSession(request))
+			return Constants.returnLogin;
+
 		log.info("Showing all categories");
-		
+
 		model.addAttribute("catList", categoryService.searchAllCategories());
-		return "categoryList";
+		return Constants.returnCategoryList;
 
 	}
 
 	/**
 	 * Handler Mapping/Method to handle delete category action by Admin
+	 * 
 	 * @param categoryId
 	 * @param model
 	 * @return
 	 */
 	@RequestMapping(value = "/deletesinglecategory.htm", method = RequestMethod.GET)
 	public String deleteSingleCategory(
-			@RequestParam(value = "catId", required = true) Long categoryId, ModelMap model, HttpServletRequest request) {
+			@RequestParam(value = "catId", required = true) Long categoryId,
+			ModelMap model, HttpServletRequest request) {
 
-		if(SessionValidateUtil.invalidSession(request))
-			return "login";
-		
+		if (SessionValidateUtil.invalidSession(request))
+			return Constants.returnLogin;
+
 		log.info("Deleting Category, Id paramater is -- " + categoryId);
-		
-		if(categoryService.deleteSingleCategory(categoryId))
-			model.addAttribute("catDelMsg", Constants.categoryDeleteSuccess);		
-		else		
+
+		if (categoryService.deleteSingleCategory(categoryId))
+			model.addAttribute("catDelMsg", Constants.categoryDeleteSuccess);
+		else
 			model.addAttribute("catDelMsg", Constants.categoryDeleteFail);
-		
+
 		model.addAttribute("catList", categoryService.searchAllCategories());
-		return "categoryList";
+		return Constants.returnCategoryList;
 	}
 
 	/**
 	 * Handler Mapping/Method to provide category details for edit
+	 * 
 	 * @param categoryId
 	 * @param model
 	 * @return
@@ -106,17 +111,20 @@ public class CategoryController {
 			@RequestParam(value = "catId", required = true) Long categoryId,
 			ModelMap model, HttpServletRequest request) {
 
-		if(SessionValidateUtil.invalidSession(request))
-			return "login";
-		
-		log.info("Editing Category, Id paramater is -- " + categoryId);		
-		
-		model.addAttribute("categoryForm", categoryService.getCategoryForEdit(categoryId));
-		return "editcategory";
+		if (SessionValidateUtil.invalidSession(request))
+			return Constants.returnLogin;
+
+		log.info("Editing Category, Id paramater is -- " + categoryId);
+
+		model.addAttribute("categoryForm",
+				categoryService.getCategoryForEdit(categoryId));
+		return Constants.returnEditCategory;
 	}
 
 	/**
-	 * Handler Mapping/Method to handle Save Category Action by Admin for new categories and update existing categories
+	 * Handler Mapping/Method to handle Save Category Action by Admin for new
+	 * categories and update existing categories
+	 * 
 	 * @param category
 	 * @param result
 	 * @param errors
@@ -130,29 +138,30 @@ public class CategoryController {
 			BindingResult result, ModelMap model, HttpServletRequest request,
 			@RequestParam(value = "userAction") String userAction) {
 
-		if(SessionValidateUtil.invalidSession(request))
-			return "login";
-		
-		log.info("Starting to save category now " + userAction);
-		
-		if (result.hasErrors())
-			return "addcategory";
-		
-		else if (userAction.equalsIgnoreCase("add") && categoryService.saveCategory(category, userAction))					
-			model.addAttribute("saveCategoryMsg", category.getCategoryName()+Constants.categoryAddSuccess);
-							
-		else if (userAction.equalsIgnoreCase("edit") && categoryService.saveCategory(category, userAction))
-		{			
-			model.addAttribute("saveCategoryMsg",category.getCategoryName()+Constants.categoryEditSuccess);			
-			return "editcategory";
-		}
-		else			
-			result.rejectValue("categoryName", "categoryName.exist",Constants.categoryExists);			
-			
-	
-		return "addcategory";
-	}	
-	
-}//End Of Controller
+		if (SessionValidateUtil.invalidSession(request))
+			return Constants.returnLogin;
 
+		log.info("Starting to save category now " + userAction);
+
+		if (result.hasErrors())
+			return Constants.returnAddCategory;
+
+		else if (userAction.equalsIgnoreCase("add")
+				&& categoryService.saveCategory(category, userAction))
+			model.addAttribute("saveCategoryMsg", category.getCategoryName()
+					+ Constants.categoryAddSuccess);
+
+		else if (userAction.equalsIgnoreCase("edit")
+				&& categoryService.saveCategory(category, userAction)) {
+			model.addAttribute("saveCategoryMsg", category.getCategoryName()
+					+ Constants.categoryEditSuccess);
+			return Constants.returnEditCategory;
+		} else
+			result.rejectValue("categoryName", "categoryName.exist",
+					Constants.categoryExists);
+
+		return Constants.returnAddCategory;
+	}
+
+}// End Of Controller
 
